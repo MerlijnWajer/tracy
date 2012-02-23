ca3c1ce7d7ad43f75a2a4859b4e5563b7d085586
Merlijn Wajer <merlijn@wizzup.org>
2012-02-23 19:29:18 +0100

    Initial work on src/main.c.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,26 @@
-int main(int argc, char** argv) {
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/reg.h>   /* For constants ORIG_EAX etc */
+#include <stdio.h>
 
+int main(int argc, char** argv) {
+    pid_t child;
+    long orig_eax;
+    child = fork();
+    if(child == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execl("/bin/ls", "ls", NULL);
+    }
+    else {
+        wait(NULL);
+        orig_eax = ptrace(PTRACE_PEEKUSER,
+                          child, 8 * ORIG_RAX,
+                          NULL);
+        printf("The child made a "
+               "system call %ld\n", orig_eax);
+        ptrace(PTRACE_CONT, child, NULL, NULL);
+    }
     return 0;
 }
