fa77ec99fee771b51ab93333a5bdd5f25a5dd30b
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-03-12 08:31:50 -0700

    implemented ipv4 and hostname for socks5

--- src/socks5/socks5.h
+++ src/socks5/socks5.h
@@ -8,47 +8,75 @@
 struct _socks5_t;
 
 typedef struct _socks5_api_t {
-	int (*socket)(int socket_family, int socket_type, int protocol,
-		struct _socks5_t *s5);
+	int (*socket)(struct _socks5_t *s5, int socket_family,
+		int socket_type, int protocol);
 
-	int (*connect)(int sockfd, const struct sockaddr *addr, socklen_t addrlen,
-		struct _socks5_t *s5);
+	int (*connect)(struct _socks5_t *s5, int sockfd,
+		const struct sockaddr *addr, socklen_t addrlen);
 	
-	int (*getaddrinfo)(const char *node, const char *service, const struct
-		addrinfo *hints, struct addrinfo **res, struct _socks5_t *s5);
+	int (*getaddrinfo)(struct _socks5_t *s5, const char *node,
+		const char *service, const struct addrinfo *hints,
+		struct addrinfo **res);
 
-	void (*freeaddrinfo)(struct addrinfo *res, struct _socks5_t *s5);
+	void (*freeaddrinfo)(struct _socks5_t *s5, struct addrinfo *res);
 
-	ssize_t (*send)(int sockfd, const void *buf, size_t len, int flags,
-		struct _socks5_t *s5);
+	ssize_t (*send)(struct _socks5_t *s5, int sockfd, const void *buf,
+		size_t len, int flags);
 	
-	ssize_t (*sendto)(int sockfd, const void *buf, size_t len, int flags,
-		const struct sockaddr *dest_addr, socklen_t addrlen,
-		struct _socks5_t *s5);
+	ssize_t (*sendto)(struct _socks5_t *s5, int sockfd, const void *buf,
+		size_t len, int flags, const struct sockaddr *dest_addr,
+		socklen_t addrlen);
 	
-	ssize_t (*recv)(int sockfd, void *buf, size_t len, int flags,
-		struct _socks5_t *s5);
+	ssize_t (*recv)(struct _socks5_t *s5, int sockfd, void *buf,
+		size_t len, int flags);
 	
-	ssize_t (*recvfrom)(int sockfd, void *buf, size_t len, int flags,
-		struct sockaddr *src_addr, socklen_t *addrlen, struct _socks5_t *s5);
+	ssize_t (*recvfrom)(struct _socks5_t *s5, int sockfd, void *buf,
+		size_t len, int flags, struct sockaddr *src_addr,
+		socklen_t *addrlen);
+	
+	int (*close)(struct _socks5_t *s5, int fd);
 } socks5_api_t;
 
 typedef struct _socks5_t {
+	// pointer to socket api implementation
 	const socks5_api_t *api;
+	// proxy server's hostname
 	const char *server;
-	int port;
-	void *arg;
+	// proxy server's port
+	unsigned short port;
+	// socket fd
 	int fd;
+	// our socks address type
+	struct {
+		// 1 = ipv4, 3 = hostname, 4 = ipv6
+		int atyp;
+		unsigned short port;
+		union {
+			struct in_addr ipv4;
+			struct in6_addr ipv6;
+			char hostname[256];
+		} u;
+	} addr;
+	// 3rd party argument
+	void *arg;
 } socks5_t;
 
 // initialize a new socks5 session
 socks5_t *socks5_init(const socks5_api_t *api, void *arg);
 
+// set the proxy server
+void socks5_set_server(socks5_t *s5, const char *server, unsigned short port);
+
 // connect to the proxy server & authorize
 int socks5_connect_proxy_server(socks5_t *s5);
 
-int socks5_connect_ipv4(socks5_t *s5, const struct sockaddr_in *addr);
+// initialize a connection to the internet
+int socks5_proxy_ipv4(socks5_t *s5, const sockaddr_in *addr);
+int socks5_proxy_ipv6(socks5_t *s5, const sockaddr_in6 *addr);
+int socks5_proxy_hostname(socks5_t *s5, const char *hostname,
+	unsigned short port);
 
-#define CB(fn, ...) s5->api->fn(__VA_ARGS__, s5)
+void socks5_close(socks5_t *s5);
+void socks5_free(socks5_t *s5);
 
 #endif
--- src/socks5/socks5_internal.h
+++ src/socks5/socks5_internal.h
@@ -1,30 +1,73 @@
 
-typedef struct _socks5_request_ipv4_t {
+//
+// SOCKS5 Request Headers
+//
+
+#define SOCKS5_REQUEST_MIN_LENGTH sizeof(socks5_request_ipv4_t)
+#define SOCKS5_REQUEST_MAX_LENGTH sizeof(socks5_request_hostname_t)
+
+typedef struct _socks5_request_bare_t {
 	unsigned char ver;
 	unsigned char cmd;
 	unsigned char rsv;
 	unsigned char atyp;
-	unsigned long addr;
+} socks5_request_bare_t;
+
+typedef struct _socks5_request_ipv4_t {
+	socks5_request_bare_t bare;
+	struct in_addr addr;
 	unsigned short port;
 } socks5_request_ipv4_t;
 
-typedef struct _socks5_request_domain_t {
-	unsigned char ver;
-	unsigned char cmd;
-	unsigned char rsv;
-	unsigned char atyp;
-	unsigned char len;
-	unsigned char hostname[];
-	// unsigned short port;
-} socks5_request_domain_t;
+typedef struct _socks5_request_ipv6_t {
+	socks5_request_bare_t bare;
+	struct in6_addr addr;
+	unsigned short port;
+} socks5_request_ipv6_t;
 
-typedef struct _socks5_reply_ipv4_t {
+typedef struct _socks5_request_hostname_t {
+	socks5_request_bare_t bare;
+	unsigned char len;
+	unsigned char hostname[255]; // maximal length of a hostname
+	unsigned short _port; // offset depends on length of `hostname'
+} socks5_request_hostname_t;
+
+//
+// SOCKS5 Reply Headers
+//
+
+#define SOCKS5_REPLY_MIN_LENGTH sizeof(socks5_reply_ipv4_t)
+#define SOCKS5_REPLY_MAX_LENGTH sizeof(socks5_reply_hostname_t)
+
+typedef struct _socks5_reply_bare_t {
 	unsigned char ver;
 	unsigned char rep;
 	unsigned char rsv;
 	unsigned char atyp;
-	unsigned long addr;
+} socks5_reply_bare_t;
+
+typedef struct __attribute__((packed)) _socks5_reply_ipv4_t {
+	socks5_reply_bare_t bare;
+	struct in_addr addr;
 	unsigned short port;
 } socks5_reply_ipv4_t;
 
+typedef struct __attribute__((packed)) _socks5_reply_ipv6_t {
+	socks5_reply_bare_t bare;
+	struct in6_addr addr;
+	unsigned short port;
+} socks5_reply_ipv6_t;
+
+typedef struct _socks5_reply_hostname_t {
+	socks5_reply_bare_t bare;
+	unsigned char len;
+	unsigned char hostname[255];
+	unsigned short _port; // do not use directly (assuming len != 255)
+} socks5_reply_hostname_t;
+
+#ifndef MIN
+inline int MIN(int a, int b) {
+	return (a < b) ? a : b;
+}
+#endif
 
