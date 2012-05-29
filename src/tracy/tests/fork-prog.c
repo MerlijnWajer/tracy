2b6bff87e5c6409a2ca614be11d012146d9ad68a
Bas Weelinck <bas.weelinck@gmail.com>
2012-05-29 11:44:54 +0200

    Safe-fork now handles clone and fork.
    - The only thing in real need of change was
      the syscall executed in the trampy code.

--- src/tracy/tests/fork-prog.c
+++ src/tracy/tests/fork-prog.c
@@ -1,15 +1,25 @@
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #include <sys/syscall.h>
 #include <unistd.h>
 
 
-int main () {
+int main (int argc, char *argv[]) {
     int child;
 
-    child = syscall(__NR_fork);
+    /* If a second argument is given
+     * fork by means of clone
+     */
+    if (argc == 2 && !strcmp("-c", argv[1])) {
+        puts("Fork by clone(2)");
+        child = fork();
+    } else {
+        puts("Fork by fork(2)");
+        child = syscall(__NR_fork);
+    }
 
     if (child == -1) {
         printf("Fork failed: %d.\n", getpid());
