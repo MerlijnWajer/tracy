dc08e405028df2c03b9724ab239aaf6478ecf39a
Bas Weelinck <bas.weelinck@gmail.com>
2012-06-12 13:21:22 +0200

    Raw-clone test for x86.

---
+++ src/tracy/tests/raw-clone.c
@@ -0,0 +1,59 @@
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sched.h>
+
+pid_t tid;
+
+/* glibc's getpid implements a cache
+ * which will cause incorrect results
+ * when calling clone(2) directly
+ */
+static pid_t no_cache_getpid()
+{
+    pid_t pid;
+
+    __asm__(
+        "int $0x80"
+        :
+            "=a"(pid)
+        :
+            "a"(__NR_getpid)
+        );
+
+    return pid;
+}
+
+/* This application executes a raw clone(2) syscall to study
+ * its return values in both parent and child.
+ * For now this test is i386 only.
+ */
+int main()
+{
+    int rval;
+    pid_t pid = 0;
+    tid = no_cache_getpid();
+
+    printf("Test initial PID (and TID) is %d\n", tid);
+
+    __asm__(
+        "int $0x80"
+        :
+            "=a"(rval)
+        :
+            "a"(__NR_clone),
+            /* Same clone arguments as glibc's fork() */
+            "b"(CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD),
+            "c"(0),     /* Child stack (not necessary for fork) */
+            "d"(&tid)   /* Child thread ID pointer */
+        );
+
+    pid = no_cache_getpid();
+    printf("In %d return value is: %d\n", pid, rval);
+    printf("In %d, TID is: %d\n", pid, tid);
+
+    return 0;
+}
+
