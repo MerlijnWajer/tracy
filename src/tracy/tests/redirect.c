c327487815817102bd2caf904510f9516403c3d9
Merlijn Wajer <merlijn@wizzup.org>
2012-07-19 13:21:19 +0200

    Added tests/redirect.c

---
+++ src/tracy/tests/redirect.c
@@ -0,0 +1,72 @@
+/*
+    This file is part of Tracy.
+
+    Tracy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tracy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tracy.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "../tracy.h"
+#include "../ll.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* For __NR_<SYSCALL> */
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#include <string.h>
+
+int hook_write(struct tracy_event *e) {
+    struct tracy_sc_args a;
+
+    if (e->child->pre_syscall) {
+        if (e->args.a0 == 2) {
+            memcpy(&a, &(e->args), sizeof(struct tracy_sc_args));
+            a.a0 = 1;
+            if (tracy_modify_syscall(e->child, a.syscall, &a)) {
+                return TRACY_HOOK_ABORT;
+            }
+        }
+    }
+
+    return TRACY_HOOK_CONTINUE;
+}
+
+int main(int argc, char** argv) {
+    struct tracy *tracy;
+
+    /* Tracy options */
+    tracy = tracy_init(TRACY_TRACE_CHILDREN);
+    tracy_set_hook(tracy, "write", hook_write);
+    /*tracy = tracy_init(TRACY_TRACE_CHILDREN | TRACY_VERBOSE);*/
+
+    if (argc < 2) {
+        printf("Usage: ./example <program-name>\n");
+        return EXIT_FAILURE;
+    }
+
+    argv++; argc--;
+
+    /* Start child */
+    if (!tracy_exec(tracy, argv)) {
+        perror("tracy_exec");
+        return EXIT_FAILURE;
+    }
+
+    /* Main event-loop */
+    tracy_main(tracy);
+
+    tracy_free(tracy);
+
+    return EXIT_SUCCESS;
+}
