2e19dcf5e082c42aba5e6b9a1b0148fa8c4f4422
Merlijn Wajer <merlijn@wizzup.org>
2013-03-06 20:09:48 +0100

    Clean amd64 arch, add x32 syscalls.

--- src/arch/amd64/arch.h
+++ src/arch/amd64/arch.h
@@ -1,10 +1,7 @@
-#define ARCH_ABI_COUNT 2
+#define ARCH_ABI_COUNT 3
 
 #define TRACY_ABI_AMD64 0
 #define TRACY_ABI_x86 1
+#define TRACY_ABI_x32 2
 
 #define TRACY_ABI_NATIVE TRACY_ABI_AMD64
-
-#if 0
-#define TRACY_ABI_x32 2
-#endif
