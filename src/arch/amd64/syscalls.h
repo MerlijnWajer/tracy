16252afe7448b32986094b5670316363ab10df18
Merlijn Wajer <merlijn@wizzup.org>
2013-09-14 12:25:38 +0200

    Full skier mode engaged

--- src/arch/amd64/syscalls.h
+++ src/arch/amd64/syscalls.h
@@ -6,12 +6,7 @@ static struct tracy_abi_syscall x86_sc[] = {
 #include "syscall_x86.h"
 };
 
-static struct tracy_abi_syscall x32_sc[] = {
-#include "syscall_x32.h"
-};
-
 struct tracy_abi_syscall* syscalls_abi[TRACY_ABI_COUNT] = {
     amd64_sc,
-    x86_sc,
-    x32_sc
+    x86_sc
 };
