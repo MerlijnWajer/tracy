b2362e411e272518d4f57bf91fb819c872019f9f
Merlijn Wajer <merlijn@wizzup.org>
2015-05-16 12:58:35 +0200

    Fixup faketime a bit.

--- src/tests/faketime.c
+++ src/tests/faketime.c
@@ -20,19 +20,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-/* For __NR_<SYSCALL> */
-#include <sys/syscall.h>
-#include <unistd.h>
+#include <time.h>
 
 #include <sys/time.h>
 
 /* TODO
- * For clock_gettime we need to be careful of the clock id -- for monotonic we
- * do not want to touch the values, for example.
- *
  * We need to think/implement the timezone value for gettimeofday
- *
  * We need to implement clock_getres(2) still
  *
  * http://www.catb.org/esr/time-programming/
@@ -66,29 +59,43 @@ int hook_open(struct tracy_event *e) {
     return TRACY_HOOK_CONTINUE;
 }
 
+/* TODO: Test hook_time -- but it should work */
 int hook_time(struct tracy_event *e) {
-    /* TODO: time(2) is broken, needs tracy_modify_args for return_code 
-     * and change pointer value */
-    if (!e->child->pre_syscall) {
-        e->args.return_code = 10000;
+    time_t t;
+
+    if (e->child->pre_syscall) {
+        e->child->custom = (void*)e->args.a0;
+    } else {
+        t = 42;
+        if (e->child->custom) {
+            tracy_write_mem(e->child, (tracy_child_addr_t)e->child->custom,
+                    (tracy_parent_addr_t)&t, sizeof(time_t));
+        }
+
+        e->args.return_code = t;
     }
     return TRACY_HOOK_CONTINUE; 
 }
 
-/* TODO: Only for CLOCK_REALTIME_COARSE CLOCK_REALTIME */
-/* TODO: Use same approach as gettimeofday */
 int hook_clock_gettime(struct tracy_event *e) {
     struct timespec tp = {2000, 0};
     int err;
 
     if (e->child->pre_syscall) {
-        e->child->custom = (void*)e->args.a1;
+        if (e->args.a0 == CLOCK_REALTIME || e->args.a0 == CLOCK_REALTIME_COARSE) {
+            e->child->custom = (void*)e->args.a1;
+        } else {
+            e->child->custom = NULL;
+        }
     } else {
-        err = tracy_write_mem(e->child, (tracy_child_addr_t)e->child->custom,
-                (tracy_parent_addr_t)&tp, sizeof(struct timespec));
-        if (err < 0) {
-            fprintf(stderr, "tracy_write_mem returned %d\n", err);
-            tracy_kill_child(e->child);
+        if (e->child->custom) {
+            err = tracy_write_mem(e->child, (tracy_child_addr_t)e->child->custom,
+                    (tracy_parent_addr_t)&tp, sizeof(struct timespec));
+
+            if (err < 0) {
+                fprintf(stderr, "tracy_write_mem returned %d\n", err);
+                tracy_kill_child(e->child);
+            }
         }
     }
 
@@ -102,11 +109,13 @@ int hook_gettimeofday(struct tracy_event *e) {
     if (e->child->pre_syscall) {
         e->child->custom = (void*)e->args.a0;
     } else {
-        err = tracy_write_mem(e->child, (tracy_child_addr_t)e->child->custom,
-                (tracy_parent_addr_t)&tv, sizeof(struct timeval));
-        if (err < 0) {
-            fprintf(stderr, "tracy_write_mem returned %d\n", err);
-            tracy_kill_child(e->child);
+        if (e->child->custom) {
+            err = tracy_write_mem(e->child, (tracy_child_addr_t)e->child->custom,
+                    (tracy_parent_addr_t)&tv, sizeof(struct timeval));
+            if (err < 0) {
+                fprintf(stderr, "tracy_write_mem returned %d\n", err);
+                tracy_kill_child(e->child);
+            }
         }
     }
 
@@ -129,6 +138,7 @@ int main(int argc, char** argv) {
     }
 
     /* Hooks */
+
     /*set_hook(stat);*/
     set_hook(open);
     set_hook(time);
